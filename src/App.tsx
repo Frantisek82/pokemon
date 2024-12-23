de1a50ac8ca95ea47d5f912e23c68a7c7cea57db
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-12-23 08:59:59 +0100

    app Pokemon inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Pokemon } from "./components/Component";
+
+interface PokemonData {
+  count: string;
+  next: string;
+  previous: string | null;
+  results: PokemonResult[];
+}
+
+// Le cambiamos el nombre a nuestro interfaz, de Pokemon a PokemonResult, ya que lo que realmente está definiendo es un resultado de la llamada al endpoint Pokemon, por eso lo llamamos PokemonResult
+interface PokemonResult {
+  name: string;
+  url: string;
+}
 
 function App() {
+  // Este valor guarda nuestra pagina actual
+  const [page, setPage] = useState(0);
+  // Este valor guarda nuestra lista de pokemons para la pagina actual
+  const [pokemons, setPokemons] = useState<PokemonResult[]>([]);
+  // Este valor guarda el 'id' del Pokemon el cual queremos mostrar los detalles
+  // Recordad que en este caso el id del Pokemon sería realmente su 'name'
+  // Se guarda sólo el 'name' porque es lo que necesitamos pasar a la siguiente petición para poder obtener los datos del Pokemon
+  const [selectedPokemonName, setSelectedPokemonName] = useState<string>();
+
+  useEffect(() => {
+    // Esta petición se realiza cada vez que 'page' cambia de valor de esta forma nos actualiza la lista de pokemon cada vez que cambiamos de página.
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page * 20}`)
+      .then((res) => res.json())
+      .then((data: PokemonData) => setPokemons(data.results));
+  }, [page]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name}>
+            {/* Creamosun botón por cada pokemon para poder selseccionarlo para mostrar sus datos al pulsarlo, lo ideal sería quizás tener un 'a' y hacer un link el próximo día cuando demos la navegación */}
+            <button onClick={() => setSelectedPokemonName(pokemon.name)}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setPage(page - 1)}>Anterior</button>
+      <button onClick={() => setPage(page + 1)}>Siguiente</button>
+      {/* Aquí comprobamos que tenemos un 'name' definido, es decir, que hemos seleccionado un Pokemón, y sólo si tenemos seleccionado un Pokemon mostramos la ficha del mismo. */}
+      {selectedPokemonName && <Pokemon name={selectedPokemonName} />}
+      {/* También se podría mostrar usando un operador ternario como aparce aquí abajo */}
+      {/* {selectedPokemonName ? <Pokemon name={selectedPokemonName} /> : null} */}
     </div>
   );
 }
